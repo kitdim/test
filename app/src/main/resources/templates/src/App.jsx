982faaa3388e47934b5226c069a5c250f76d2896
kitdim <kitaykin9808@gmail.com>
2023-11-12 12:58:13 +0300

    init commit

---
+++ app/src/main/resources/templates/src/App.jsx
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+import "./App.css";
+import EndOfGame from "./components/EndOfGame/EndOfGame";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Rules from "./components/Rules/Rules";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Rules />
+      <EndOfGame />
+      <Footer />
+=======
+import './App.css'
+import PuzzleBoard from './components/puzzle-board/puzzle-board'
+
+function App() {
+
+  return (
+    <>
+    <PuzzleBoard></PuzzleBoard>
+>>>>>>> a4f75659c197eb7a0d9d61b1824c1e0a03183cdd
+    </>
+  );
+}
+
+export default App;
